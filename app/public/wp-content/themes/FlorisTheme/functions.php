9f003f50b16654be9bcb9aa4fe3b886fb9305200
Floris van Drunen <floris.van.drunen@outlook.com>
2025-10-08 11:32:34 +0200

    updated

--- app/public/wp-content/themes/FlorisTheme/functions.php
+++ app/public/wp-content/themes/FlorisTheme/functions.php
@@ -24,8 +24,6 @@ function loadCss()
 }
 add_action('wp_enqueue_scripts', 'loadCss');
 
-// --- FlorisTheme additions ---
-
 // FlorisTheme - functions.php (added menu and sidebar registration)
 if ( ! function_exists( 'floris_setup' ) ) {
     function floris_setup() {
